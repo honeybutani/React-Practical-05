05b67225506375cb85d3101544c709532e97befe
honeybutani <honey.b@simformsolutions.com>
2022-04-18 15:36:26 +0530

    delete functionality issue resolved

--- src/components/CardHover.js
+++ src/components/CardHover.js
@@ -4,20 +4,23 @@ import { useSelector } from "react-redux";
 export default function CardHover() {
   /* on hover the user get detail information about user in side card  */
   const users = useSelector((state) => state.users);
-  const id = useSelector((state) => state.selectedid - 1);
+  const id = useSelector((state) => state.selectedid);
+  const index = users.findIndex(user => user.id === id);
+
+  console.log(index);
   return (
     <div>
       <div className="card HoverCard">
         <img
           className=" img CardCenterContent"
-          src={users[id].image}
+          src={users[index].image}
           alt="not display"
           width="115"
           height="115"
         />
 
-        <div className="name CardCenterContent">{users[id].firstName}</div>
-        <div className=" email CardCenterContent"> {users[id].email}</div>
+        <div className="name CardCenterContent">{users[index].firstName}</div>
+        <div className=" email CardCenterContent"> {users[index].email}</div>
 
         <div className="Plan CardCenterContent">
           <b>Your Plan : Standard</b>
@@ -39,12 +42,12 @@ export default function CardHover() {
         </div>
         <div className="clicks">
           <div className="clicksnumber">
-            <b className="clicksreviewed">{users[id].clicksreviewed}</b>
+            <b className="clicksreviewed">{users[index].clicksreviewed}</b>
             <br /> clicks reviewed
           </div>
 
           <div className="betweenborder">
-            <b className="monthlyclicks">{users[id].monthlyclicked}</b>
+            <b className="monthlyclicks">{users[index].monthlyclicked}</b>
             <br /> Monthly clicks
           </div>
         </div>
